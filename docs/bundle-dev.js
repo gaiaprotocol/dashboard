edc96a516083f34da4c8abf1a7a28fbf52e1d490
yjgaia <yjdev88@gmail.com>
2025-03-20 20:37:10 +0900

    feat: update init function calls to use await for asynchronous execution

--- docs/bundle-dev.js
+++ docs/bundle-dev.js
@@ -36,7 +36,7 @@ eval("__webpack_require__.a(module, async (__webpack_handle_async_dependencies__
   \********************/
 /***/ ((module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.a(module, async (__webpack_handle_async_dependencies__, __webpack_async_result__) => { try {\n__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _init_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./init.js */ \"./app/init.ts\");\nvar __webpack_async_dependencies__ = __webpack_handle_async_dependencies__([_init_js__WEBPACK_IMPORTED_MODULE_0__]);\n_init_js__WEBPACK_IMPORTED_MODULE_0__ = (__webpack_async_dependencies__.then ? (await __webpack_async_dependencies__)() : __webpack_async_dependencies__)[0];\n\n(0,_init_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])({\n    isDevMode: true,\n});\n\n__webpack_async_result__();\n} catch(e) { __webpack_async_result__(e); } });\n\n//# sourceURL=webpack:///./app/dev.ts?");
+eval("__webpack_require__.a(module, async (__webpack_handle_async_dependencies__, __webpack_async_result__) => { try {\n__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _init_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./init.js */ \"./app/init.ts\");\nvar __webpack_async_dependencies__ = __webpack_handle_async_dependencies__([_init_js__WEBPACK_IMPORTED_MODULE_0__]);\n_init_js__WEBPACK_IMPORTED_MODULE_0__ = (__webpack_async_dependencies__.then ? (await __webpack_async_dependencies__)() : __webpack_async_dependencies__)[0];\n\nawait (0,_init_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])({\n    isDevMode: true,\n});\n\n__webpack_async_result__();\n} catch(e) { __webpack_async_result__(e); } }, 1);\n\n//# sourceURL=webpack:///./app/dev.ts?");
 
 /***/ }),
 
